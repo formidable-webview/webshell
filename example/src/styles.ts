9cd1c6edc25563d65568b1862dcbfd73913e04e4
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2020-09-07 13:57:07 -0300

    feat: wip v2

--- example/src/styles.ts
+++ example/src/styles.ts
@@ -1,4 +1,4 @@
-export const BOTTOM_SHEET_CONTENT_HEIGHT = 450;
+export const BOTTOM_SHEET_CONTENT_HEIGHT = 380;
 export const STAT_HEIGHT = 55;
 export const TOP_TEXT_HEIGHT = 60;
 export const APPBAR_HEIGHT = 45;
