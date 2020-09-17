8a3d7c9cd0a4415dc58ba13a6ca839f7a1ea7dc8
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2020-09-17 17:36:47 -0300

    feat: wip documentation

--- website/docs/APIBox.jsx
+++ website/docs/APIBox.jsx
@@ -3,7 +3,7 @@ import { APIReference } from './APIReference';
 
 export const APIBox = (props) => {
   return (
-    <div className="card api-box shadow--tl" style={{ textAlign: 'center' }}>
+    <div className="card api-box shadow--lt" style={{ textAlign: 'center' }}>
       <div className={'card__header api-box__title'}>~ API Reference ~</div>
       <APIReference {...props} />
     </div>
