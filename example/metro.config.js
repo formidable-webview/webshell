ef77eb7b3e882eb71b22576d3a1704717496bd61
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2020-09-23 15:15:22 -0300

    feat: wip workspace:

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,43 +1,24 @@
 const path = require('path');
+const { getDefaultConfig } = require('metro-config');
 const blacklist = require('metro-config/src/defaults/blacklist');
-const escape = require('escape-string-regexp');
-const pak = require('../package.json');
 
 const root = path.resolve(__dirname, '..');
-
 const modules = Object.keys({
-  ...pak.peerDependencies
+  ...require('../package.json').peerDependencies
 });
-
-const metroDefault = require('metro-config/src/defaults/defaults.js');
 
 module.exports = {
   projectRoot: __dirname,
-  watchFolders: [root],
-
-  // We need to make sure that only one version is loaded for peerDependencies
-  // So we blacklist them at the root, and alias them to the versions in example's node_modules
+  watchFolders: [path.resolve(root, 'src')],
   resolver: {
-    sourceExts: metroDefault.sourceExts.concat(['webjs', 'html']),
-    // blacklistRE: blacklist(
-    //   modules.map(
-    //     (m) =>
-    //       new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`)
-    //   )
-    // ),
-
-    // extraNodeModules: modules.reduce(
-    //   (acc, name) => {
-    //     acc[name] = path.join(__dirname, 'node_modules', name);
-    //     return acc;
-    //   },
-      // {
-      //   '@formidable-webview/webshell': root
-      // }
-    // )
+    sourceExts: ['js', 'json', 'ts', 'tsx', 'webjs', 'html']
   },
-
   transformer: {
+    // extraNodeModules: [
+    //   {
+    //     '@formidable-webview/webshell': root
+    //   }
+    // ],
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
@@ -46,3 +27,38 @@ module.exports = {
     })
   }
 };
+
+// module.exports = async () => {
+//   const defaultConfig = await getDefaultConfig();
+//   return {
+//     projectRoot: __dirname,
+//     watchFolders: [root],
+//     resolver: {
+//       sourceExts: defaultConfig.resolver.sourceExts.concat(['webjs', 'html']),
+//       blacklistRE: blacklist(
+//         modules.map(
+//           (m) =>
+//             new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`)
+//         )
+//       ),
+//       extraNodeModules: modules.reduce(
+//         (acc, name) => {
+//           acc[name] = path.join(__dirname, 'node_modules', name);
+//           return acc;
+//         },
+//         {
+//           '@formidable-webview/webshell': root
+//         }
+//       ),
+//       transformer: {
+//         getTransformOptions: async () => ({
+//           transform: {
+//             experimentalImportSupport: false,
+//             inlineRequires: true
+//           }
+//         })
+//       }
+//     }
+//   };
+//   //.then(() => Promise.reject(new Error('Prevent start')));
+// };
