034010a312094b41ce9c94c3504ecb9e6925ba32
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2020-09-25 16:54:19 -0300

    test: implement features tests

---
+++ packages/core/src/features/__tests__/HandleElementCSSBoxFeature.test.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import Ersatz from '@formidable-webview/ersatz';
+import makeErsatzTesting from '@formidable-webview/ersatz-testing';
+import { render } from '@testing-library/react-native';
+import { makeWebshell } from '../../make-webshell';
+import {
+  HandleElementCSSBoxFeature,
+  ElementCSSBoxDimensions,
+  CSSBox
+} from '../HandleElementCSSBoxFeature';
+
+const { waitForErsatz } = makeErsatzTesting(Ersatz);
+
+const boxMatcher: Record<keyof CSSBox, any> = {
+  width: expect.any(Number),
+  height: expect.any(Number)
+};
+const dimensionsMatcher: Record<keyof ElementCSSBoxDimensions, any> = {
+  borderBox: boxMatcher,
+  scrollBox: boxMatcher,
+  computedStyle: expect.any(Object),
+  horizontalScrollbarWidth: expect.any(Number),
+  verticalScrollbarWidth: expect.any(Number)
+};
+
+describe('Webshell with HandleElementCSSBoxFeature', () => {
+  it('should invoke onDOMElementCSSBoxDimensions prop when the DOM is mounted', async () => {
+    const onDOMElementDimensions = jest.fn();
+    const html = `
+    <table>
+    <tr>
+      <th>Entry Header 1</th>
+      <th>Entry Header 2</th>
+      <th>Entry Header 3</th>
+    </tr>
+    </table>
+    `;
+    const Webshell = makeWebshell(
+      Ersatz,
+      new HandleElementCSSBoxFeature({ target: { tagName: 'table' } })
+    );
+    await waitForErsatz(
+      render(
+        <Webshell
+          onDOMElementCSSBoxDimensions={onDOMElementDimensions}
+          source={{ html }}
+        />
+      )
+    );
+    expect(onDOMElementDimensions).toHaveBeenCalledWith(dimensionsMatcher);
+  });
+});
---
+++ packages/core/src/features/__tests__/HandleLinkPressFeature.test.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import Ersatz from '@formidable-webview/ersatz';
+import makeErsatzTesting from '@formidable-webview/ersatz-testing';
+import { render } from '@testing-library/react-native';
+import { makeWebshell } from '../../make-webshell';
+import {
+  HandleLinkPressFeature,
+  LinkPressTarget
+} from '../HandleLinkPressFeature';
+
+const { waitForDocument } = makeErsatzTesting(Ersatz);
+
+describe('Webshell with HandleLinkPressFeature', () => {
+  it('should invoke onDOMLinkPress prop when a link is pressed', async () => {
+    const onDOMLinkPress = jest.fn();
+    const Webshell = makeWebshell(Ersatz, new HandleLinkPressFeature());
+    const document = await waitForDocument(
+      render(
+        <Webshell
+          onDOMLinkPress={onDOMLinkPress}
+          source={{ html: '<a id="anchor0" href="https://foo.org">bar</a>' }}
+        />
+      )
+    );
+    document.getElementById('anchor0').click();
+    expect(onDOMLinkPress).toHaveBeenCalledWith({
+      uri: 'https://foo.org/',
+      scheme: expect.any(String),
+      hrefAttribute: expect.any(String),
+      clickedAnchorBoundingRect: {
+        top: expect.any(Number),
+        left: expect.any(Number),
+        right: expect.any(Number),
+        bottom: expect.any(Number),
+        width: expect.any(Number),
+        height: expect.any(Number)
+      },
+      page: expect.any(Object)
+    } as Record<keyof LinkPressTarget, any>);
+  });
+});
