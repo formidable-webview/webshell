7d90f6ca21db5ded6dd27ef96b0aca06219d75e7
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2020-09-25 16:53:38 -0300

    refactor: move features index.ts to the new workspace

---
+++ packages/core/src/features/index.ts
@@ -0,0 +1,7 @@
+export * from './HandleElementCSSBoxFeature';
+export * from './HandleHTMLDimensionsFeature';
+export * from './HandleLinkPressFeature';
+export * from './HandleHashChangeFeature';
+export * from './HandleVisualViewportFeature';
+export * from './ForceResponsiveViewportFeature';
+export * from './ForceElementSizeFeature';
