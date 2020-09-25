873d6468fb5e5a9e1fbf807acd52ecb307d5c07f
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2020-09-25 16:46:37 -0300

    feat: new DOM feature HandleElementCSSBoxFeature
    BREAKING CHANGE: elementSizeFeature has been replaced with its
    instanciable counterpart, HandleElementCSSBoxFeature. If you need the
    whole document size, consider HandleHTMLDimensionsFeature.

---
+++ packages/core/src/features/HandleElementCSSBoxFeature.ts
@@ -0,0 +1,141 @@
+import script from './HandleElementCSSBoxFeature.webjs';
+import { FeatureBuilder } from '../FeatureBuilder';
+import type { DOMElementRequest, PropDefinition } from '../types';
+import type { FeatureConstructor } from '../Feature';
+
+/**
+ * An object describing customization for the dimensions feature.
+ *
+ * @public
+ */
+export interface HandleElementCSSBoxDimensionsOptions {
+  /**
+   * The element to target. This argument is required.
+   */
+  target: DOMElementRequest;
+  /**
+   * When no elements are found matching the target, should the script
+   * raise an error?
+   *
+   * @defaultValue false
+   */
+  shouldThrowWhenNotFound?: boolean;
+}
+
+/**
+ * @public
+ */
+export interface CSSBox {
+  width: number;
+  height: number;
+}
+
+/**
+ * Computed styles which affect the CSS Box dimensions.
+ * See {@link https://developer.mozilla.org/docs/Web/API/Window/getComputedStyle | window.getComputedStyle()}.
+ *
+ * @public
+ */
+export interface CSSBoxDimensionsComputedStyle {
+  paddingTop: number;
+  paddingBottom: number;
+  paddingLeft: number;
+  paddingRight: number;
+  borderTopWidth: number;
+  borderBottomWidth: number;
+  borderLeftWidth: number;
+  borderRightWidth: number;
+  marginTop: number;
+  marginBottom: number;
+  marginLeft: number;
+  marginRight: number;
+}
+
+/**
+ * An object describing an element CSS Box dimensions, see
+ * {@link https://drafts.csswg.org/css2/#box-model | CSS 2 (Box model)}.
+ *
+ * @remarks
+ *
+ * This object scalar units are CSS pixels.
+ *
+ * @public
+ */
+export interface ElementCSSBoxDimensions {
+  /**
+   * A box formed by `scrollWidth` and `scrollHeight` element properties.
+   *
+   * @remarks
+   * The box is formed with all the space occupied by element's children, even
+   * when overflowing. The element padding, border and scrollbar are not
+   * counted. See
+   * {@link https://drafts.csswg.org/cssom-view/#dom-element-scrollwidth},
+   * `scrollWidth` and `scrollHeight` for a reference.
+   */
+  scrollBox: CSSBox;
+  /**
+   * The border box as specified in the
+   * {@link https://drafts.csswg.org/css-box-3/#valdef-box-border-box | CSS Box Model}.
+   *
+   * @remarks
+   * Margin, padding and content boxes can be derived from
+   * {@link ElementCSSBoxDimensions.computedStyle}.
+   */
+  borderBox: CSSBox;
+  /**
+   * The computed box style. See
+   * {@link https://developer.mozilla.org/docs/Web/API/Window/getComputedStyle | window.getComputedStyle()}.
+   *
+   * @remarks
+   * Be aware that the computed vertical margins might collapse in the
+   * viewport. See
+   * {@link https://drafts.csswg.org/css2/#collapsing-margins | CSS 2 (collapsing margins)}
+   */
+  computedStyle: CSSBoxDimensionsComputedStyle;
+  /**
+   * The width of the horizontal scrollbar.
+   *
+   * @remarks
+   * In the CSS Box model, scrollbars are part of the content box.
+   */
+  horizontalScrollbarWidth: number;
+  /**
+   * The width of the vertical scrollbar.
+   *
+   * @remarks
+   * In the CSS Box model, scrollbars are part of the content box.
+   */
+  verticalScrollbarWidth: number;
+}
+
+const defaultOptions: HandleElementCSSBoxDimensionsOptions = {
+  shouldThrowWhenNotFound: false
+} as HandleElementCSSBoxDimensionsOptions;
+
+/**
+ * This feature enables receiving the CSS Box dimensions of an element
+ * identified by `tagName` in the `WebView` pixels unit. The first element
+ * matching the provided tagName is retained. A new event will be triggered on
+ * every resize.
+ *
+ * @public
+ */
+export const HandleElementCSSBoxFeature: FeatureConstructor<
+  HandleElementCSSBoxDimensionsOptions,
+  [
+    PropDefinition<{
+      onDOMElementCSSBoxDimensions?: (e: ElementCSSBoxDimensions) => void;
+    }>
+  ]
+> = new FeatureBuilder({
+  script,
+  defaultOptions,
+  className: 'HandleElementCSSBoxFeature',
+  featureIdentifier:
+    'org.formidable-webview/webshell.handle-element-cssbox-dimensions'
+})
+  .withEventHandlerProp<
+    ElementCSSBoxDimensions,
+    'onDOMElementCSSBoxDimensions'
+  >('onDOMElementCSSBoxDimensions')
+  .build();
