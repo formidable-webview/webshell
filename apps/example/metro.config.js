bc8cf9e7499a59de1d13e76d9d95d3fa6b4ef6b0
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2020-10-02 07:48:13 -0300

    chore: simplify metro config with async module export

--- apps/example/metro.config.js
+++ apps/example/metro.config.js
@@ -1,28 +1,25 @@
 const path = require('path');
 const fs = require('fs');
+const { getDefaultConfig } = require('metro-config');
 
 const packagesRoot = path.resolve(__dirname, '../../packages');
 
 const localPkgs = fs.readdirSync(packagesRoot);
 
-module.exports = {
-  projectRoot: __dirname,
-  watchFolders: localPkgs.map((f) => path.join(packagesRoot, f)),
-  resolver: {
-    sourceExts: ['js', 'json', 'ts', 'tsx', 'webjs', 'html'],
-    extraNodeModules: new Proxy(
-      {},
-      {
-        get: (target, name) => path.join(__dirname, `node_modules/${name}`)
-      }
-    )
-  },
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true
-      }
-    })
-  }
-};
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts }
+  } = await getDefaultConfig();
+  return {
+    watchFolders: localPkgs.map((f) => path.join(packagesRoot, f)),
+    resolver: {
+      sourceExts: [...sourceExts, 'webjs', 'html'],
+      extraNodeModules: new Proxy(
+        {},
+        {
+          get: (target, name) => path.join(__dirname, `node_modules/${name}`)
+        }
+      )
+    }
+  };
+})();
